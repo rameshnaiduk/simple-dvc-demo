d8a69779fc6233b7c4e64a1701be0555a9c708b4
naiduk1990 <naiduk1990@gmail.com>
2021-08-12 14:11:34 +0530

    Spliting is over'

--- src/get_data.py
+++ src/get_data.py
@@ -23,7 +23,7 @@ def get_data(config_path):
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default='params.yaml')
+    args.add_argument("--config" , default='params.yaml')
     parsed_args = args.parse_args()
     data = get_data(config_path=parsed_args.config)
 
