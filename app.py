3d4d75da656af4202c1bbe14aa0cd5b427ebf33b
naiduk1990 <naiduk1990@gmail.com>
2021-08-24 15:40:39 +0530

    App is working

--- app.py
+++ app.py
@@ -11,28 +11,53 @@ webapp_root = "webapp"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+def read_param(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config = read_param(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    predict = model.predict(data)
+    print(predict)
+    return predict[0]
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json,values())])
+        response = predict(data)
+        response = {"response":response}
+        return response
+
+    except Exception as e:
+        print(e)
+        error = {"error": "Somethong went wrong!!"}
+        return error
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-
     if request.method == "POST":
         try:
             if request.form:
-                dict_req = dict(request.form)
-                response = prediction.form_response(dict_req)
-                return render_template("index.html", response=response)
+                data = dict(request.form).values()
+                data= [list(map(float,data))]
+                response = predict(data)
+                return render_template("index.html",response=response)
+
             elif request.json:
-                response = prediction.api_response(request.json)
+                response = api_response(request)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "Something went wrong!! Try again later!"}
             error = {"error": e}
+            return render_template("404.html", error=e)
 
-            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
